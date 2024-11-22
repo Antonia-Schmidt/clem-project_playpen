fcc1720cc9b48cb325c44961d7ee163ff4659515
Nicola Horst <nic-er.ht@gmx.de>
2024-11-22 10:16:54 +0000

    added print

--- run_gridsearch.sh
+++ run_gridsearch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 ./train_model_qlora_gridsearch.py --model_name "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit" --output_dir './output' --training_dataset './data/training_data/D30003.csv' --model_adapter ''
+python3 ./train_model_qlora_gridsearch.py --model_name "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit" --output_dir './output' --training_dataset './data/training_data/D30001.csv' --model_adapter ''
 
