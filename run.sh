ba66ae1f8b8c3901551e83098670b69fdea5f4bb
NicolaHorst <nic-er.ht@gmx.de>
2024-12-23 15:00:29 +0000

    added d9000

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-python3 ./train_model_qlora.py --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" --output_dir './output' --training_dataset './data/training_data/D70001.csv' --model_adapter ''
-python3 ./train_model_qlora.py --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" --output_dir './output' --training_dataset './data/training_data/D70002.csv' --model_adapter ''
-python3 ./train_model_qlora.py --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" --output_dir './output' --training_dataset './data/training_data/D70003.csv' --model_adapter ''
-python3 ./train_model_qlora.py --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" --output_dir './output' --training_dataset './data/training_data/D70005.csv' --model_adapter ''
+python3 ./train_model_qlora.py --model_name "meta-llama/Meta-Llama-3.1-8B-Instruct" --output_dir './output' --training_dataset './data/training_data/D90000.csv' --model_adapter ''
