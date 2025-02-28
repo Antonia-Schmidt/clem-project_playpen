7a909280005e55b29ccf2b7b44f28d0003e12751
NicolaHorst <nic-er.ht@gmx.de>
2025-02-28 15:42:47 +0000

    removed more

--- run_dpo_training.sh
+++ run_dpo_training.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --neg 1 --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --neg 2 --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --neg 3 --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --neg 6 --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --model_condition best_models --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
-CUDA_VISIBLE_DEVICES=0 python3 DPO_training.py --model_condition same_family_model --hf_login "" --wandb_login "" --cache_dir ./output/DPO_29_01_2025
