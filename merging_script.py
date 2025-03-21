230367dbb72079f74c3782fc735e30930aea83b2
NicolaHorst <nic-er.ht@gmx.de>
2025-03-21 23:08:15 +0000

    added adjusted mergin script

--- merging_script.py
+++ merging_script.py
@@ -1,5 +1,4 @@
 from unsloth import FastLanguageModel
-from huggingface_hub import login
 
 model_to_merge = 'llama-3.1-70B-Instruct_playpen_SFT_DFINAL_0.6K-steps'
 
@@ -7,5 +6,5 @@ model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = f"clembench-playpen/{model_to_merge}",
 )
 
-model.save_pretrained_merged("model", tokenizer, save_method = "merged_16bit")
+#model.save_pretrained_merged("model", tokenizer, save_method = "merged_16bit")
 model.push_to_hub_merged(f"clembench-playpen/{model_to_merge}_merged_fp16", tokenizer, save_method = "merged_16bit")
